8f3f1b1f143cb30566bca1b572aebc107a31e1a3
052_arteddy bukit <89283796+arteddybukit@users.noreply.github.com>
2022-11-10 13:49:04 +0800

    Add files via upload

---
+++ Tugas UTS/storage/framework/views/8ffd670229cf07f3487b99fc3f8216332102e799.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="<?php echo e(csrf_token()); ?>">
+    
+    <title><?php echo e(config('app.name', 'Laravel')); ?></title>
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;6
+00;700&display=swap">
+    <!-- Scripts -->
+    
+    <link rel="stylesheet" href="<?php echo e(asset('css/app.css')); ?>">
+    <script src="<?php echo e(asset('js/app.js')); ?>" defer></script>
+    
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/twelements/dist/css/index.min.css" />
+    <script src="https://cdn.jsdelivr.net/npm/tw-elements/dist/js/index.min.js"></script>
+</head>
+
+<body class="font-sans antialiased">
+    <div class="min-h-screen bg-gray-100">
+        <?php echo $__env->make('layouts.navigationhome', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+        <!-- Page Content -->
+        <main>
+            <?php echo e($slot); ?>
+
+        </main>
+    </div>
+</body>
+
+</html><?php /**PATH C:\xampp\htdocs\kecak\resources\views/layouts/home-layout.blade.php ENDPATH**/ ?>
