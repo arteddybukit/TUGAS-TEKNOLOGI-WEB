2b1aa2d8e416156050131398cd575a1c76ea5331
ARTEDDY <arteddy.bukit@gmail.com>
2022-11-10 17:18:39 +0800

    add files via upload

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+//memanggil file controller
+use App\Http\Controllers\HomeController;
+//route sebelumnya
+// Route::get('/', function () {
+// return view('frontpage.landingpage');
+// });
+//route yang terbaru
+Route::get('/', [HomeController::class, 'index']);
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+Route::get('/order', function () {
+    return view('order');
+});
+
+require __DIR__ . '/auth.php';
